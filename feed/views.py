66727b1ee4f4a4e861e6b4a1fa2de057103dd882
Prithvi Raj M <prithvirajm2003@gmail.com>
2024-08-28 15:13:15 +0530

    Added Create Post Function and made Login Mandatory to create post

--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomePage(ListView):
     http_method_names=["get"]
@@ -7,3 +9,14 @@ class HomePage(ListView):
     model=Post
     context_object_name="posts"
     queryset=Post.objects.all().order_by('-id')[0:30]
+
+class PostDetailView(DetailView):
+    http_method_names=["get"]
+    template_name="feed/detail.html"
+    model=Post
+    context_object_name="post"
+
+class CreateNewPost(LoginRequiredMixin, CreateView):
+    model=Post
+    template_name="feed/create.html"
+    fields=['text']
